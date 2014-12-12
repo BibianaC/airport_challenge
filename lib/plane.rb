419315a82a106174ae3d743acb0319c3d9d4832e
bibiana <bcristofol@gmail.com>
2014-12-12 17:42:38 +0000

    Plane class - test flying and landing status

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,24 @@
 class Plane
 
   def initialize
-    flying?()
+    in_air!()
   end
 
   def flying?
+    @fly
+  end
+
+  def in_air!
     @fly = true
   end
+
+  def land!
+    @fly = false
+  end
+
+  def landed?
+    !@fly
+  end
+
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -8,4 +8,21 @@ describe Plane do
     expect(plane).to be_flying
   end
 
+  it "should have a flying status when in the air" do
+    plane.in_air!
+    expect(plane).to be_flying
+  end
+
+  it "can land" do
+    expect(plane).to be_flying
+    plane.land!
+    expect(plane).not_to be_flying
+  end
+
+  it "should change status to landed after landing" do
+    plane.land!
+    expect(plane).to be_landed
+  end
+
+
 end
