c700a89d4d0a187b0f52a7fe27cb0ac5964f639d
bibiana <bcristofol@gmail.com>
2014-12-14 14:42:09 +0000

    Plane class - Added context

--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -4,36 +4,48 @@ describe Plane do
 
   let(:plane) {Plane.new}
 
-  it "should have a flying status when created" do
-    expect(plane).to be_flying
+  context 'flying status' do
+
+    it "should have a flying status when created" do
+      expect(plane).to be_flying
+    end
+
+    it "should have a flying status when in the air" do
+      plane.in_air!
+      expect(plane).to be_flying
+    end
+
+    it "should change status to flying after taking off" do
+      plane.take_off!
+      expect(plane).to be_flying
+    end
+
   end
 
-  it "should have a flying status when in the air" do
-    plane.in_air!
-    expect(plane).to be_flying
+  context 'taking off and landing' do
+
+    it "can land" do
+      expect(plane).to be_flying
+      plane.land!
+      expect(plane).not_to be_flying
+    end
+
+    it "can take off" do
+      plane.land!
+      expect(plane).to be_landed
+      plane.take_off!
+      expect(plane).not_to be_landed
+    end
+
   end
 
-  it "can land" do
-    expect(plane).to be_flying
-    plane.land!
-    expect(plane).not_to be_flying
-  end
+  context 'landing status' do
 
-  it "should change status to landed after landing" do
-    plane.land!
-    expect(plane).to be_landed
-  end
+    it "should change status to landed after landing" do
+      plane.land!
+      expect(plane).to be_landed
+    end
 
-  it "can take off" do
-    plane.land!
-    expect(plane).to be_landed
-    plane.take_off!
-    expect(plane).not_to be_landed
-  end
-
-  it "should change status to flying after taking off" do
-    plane.take_off!
-    expect(plane).to be_flying
   end
 
 end
