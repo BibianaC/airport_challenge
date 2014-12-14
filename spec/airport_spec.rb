3254eb2121da922349637971ee1f38a1ee40bcdc
bibiana <bcristofol@gmail.com>
2014-12-14 14:34:18 +0000

    Airport class - Added context

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -6,35 +6,47 @@ describe Airport do
   let(:plane) {double :plane}
   let(:weather) {double :weather}
 
-  it "should allow planes to land" do
-    expect(airport.plane_count).to eq(0)
-    allow(airport).to receive(:stormy?) {false}
-    airport.receive(plane)
-    expect(airport.plane_count).to eq(1)
+  context 'taking off and landing' do
+
+    it "should allow planes to land" do
+      expect(airport.plane_count).to eq(0)
+      allow(airport).to receive(:stormy?) {false}
+      airport.receive(plane)
+      expect(airport.plane_count).to eq(1)
+    end
+
+    it "should allow planes to take off" do
+      allow(airport).to receive(:stormy?) {false}
+      airport.receive(plane)
+      airport.release(plane)
+      expect(airport.plane_count).to eq(0)
+    end
+
   end
 
-  it "should allow planes to take off" do
-    allow(airport).to receive(:stormy?) {false}
-    airport.receive(plane)
-    airport.release(plane)
-    expect(airport.plane_count).to eq(0)
+  context 'traffic control' do
+
+    it "should not allow planes to land when it is full" do
+      allow(airport).to receive(:stormy?) {false}
+      100.times {airport.receive(:plane)}
+      expect(airport).to be_full
+      expect(lambda {airport.receive(:plane)}).to raise_error(RuntimeError, 'Airport is full')
+    end
+
   end
 
-  it "should not allow planes to land when it is full" do
-    allow(airport).to receive(:stormy?) {false}
-    100.times {airport.receive(:plane)}
-    expect(airport).to be_full
-    expect(lambda {airport.receive(:plane)}).to raise_error(RuntimeError, 'Airport is full')
-  end
+  context 'weather conditions' do
 
-  it "should not allow planes to land when it is stormy" do
-    allow(airport).to receive(:stormy?) {true}
-    expect(lambda {airport.receive(:plane)}).to raise_error('You cannot land, it is too stormy')
-  end
+    it "should not allow planes to land when it is stormy" do
+      allow(airport).to receive(:stormy?) {true}
+      expect(lambda {airport.receive(:plane)}).to raise_error('You cannot land, it is too stormy')
+    end
 
-  it "should not allow planes to take off when it is stormy" do
-    allow(airport).to receive(:stormy?) {true}
-    expect(lambda {airport.release(:plane)}).to raise_error('You cannot take off, it is too stormy')
+    it "should not allow planes to take off when it is stormy" do
+      allow(airport).to receive(:stormy?) {true}
+      expect(lambda {airport.release(:plane)}).to raise_error('You cannot take off, it is too stormy')
+    end
+
   end
 
 end
